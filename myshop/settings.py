7f13118cdcf34e4e8a0aeb01ec6fff548d99f025
Olalekan Ogundele <olalekan.o.ogundele@gmail.com>
2023-01-06 14:23:36 +0100

    Integrated payments

--- myshop/settings.py
+++ myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "shop.apps.ShopConfig",
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
+    "payment.apps.PaymentConfig",
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -130,5 +132,21 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CART_SESSION_ID = 'cart'
+
+# Email Server configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'admin@pardise.com'
+EMAIL_HOST_PASSWORD = 'xgdkjskjawbizfwl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Stripe settings
+STRIPE_PUBLISHABLE_KEY = 'pk_test_51MN86EK5GsdCL3nX9slLBdvsu1IiYUJY1Lb81mnKdPjebggmRKk8ycApq0SsE0oSx77nH9RqIyiPwgARgnwrSoX000Y9GbPmsC'             # Publishable Key
+STRIPE_SECRET_KEY = 'sk_test_51MN86EK5GsdCL3nXQj3edOjhP5jO8VKG7h0U8bVIexk9uQfCWjxOWICC6wwV2D8jIUzXKUYCZi5MMAHwH0b6xFkD006JANwCmV'                  # Secret Key
+STRIPE_API_VERSION = '2022-08-01'
+STRIPE_WEBHOOK_SECRET = "whsec_ce99d23fdee0c08b57ec9242527c19b9bd41f602421fccab95f4fa68ad7197c0"
+
+
 
 
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
+from .tasks import order_created
+from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 
 
 def order_create(request):
@@ -19,11 +26,36 @@ def order_create(request):
 
             # clear the cart
             cart.clear()
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            # launch asynchronous task
+            order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
         return render(request,
                       'orders/order/create.html',
                       {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(
+        settings.STATIC_ROOT / 'css/pdf.css'
+    )])
+    return response
+
